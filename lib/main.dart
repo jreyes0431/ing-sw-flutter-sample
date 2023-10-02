145b118e2452505a3d2d6482c41f57a2c663fd2d
jreyes0431 <jose-31.31@hotmail.com>
2023-10-02 12:53:10 -0500

    feat: clean start up

--- lib/main.dart
+++ lib/main.dart
@@ -9,46 +9,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
-        ),
-        appBar: AppBar(
-          title: const Text('Mi primera app'),
-        ),
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(50),
-                child: Text(
-                  "Bienvenido a mi primera app",
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: const Text("Presioname"),
-              ),
-              TextFormField(
-                decoration: InputDecoration(hintText: "Ingresa tu ID"),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("Ingresar"),
-              )
-            ],
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: Scaffold(),
     );
   }
 }
