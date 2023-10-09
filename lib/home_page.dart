67fb6a15f0794443bf3b47f4dbd19308d0559ee6
jreyes0431 <jose-31.31@hotmail.com>
2023-10-09 12:38:49 -0500

    refactor: home page comments

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,9 +12,11 @@ class HomePage extends StatelessWidget {
 
   //Esto es solo una función que retorna un Widget
   //Recibe como parametro un String y una función
+  //Esto no es lo adecuado, sin embargo es una forma de hacerlo sencillo
   Widget customButton(String text, BuildContext context) {
     return ElevatedButton(
       onPressed: () {
+        //Esto es lo que sucederá cada vez que se presione uno de estos botones
         Navigator.push(
           context,
           MaterialPageRoute(
@@ -23,6 +25,7 @@ class HomePage extends StatelessWidget {
         );
       },
       style: ButtonStyle(
+        //acá le damos estilo al botón
         minimumSize: const MaterialStatePropertyAll(Size(180, 10)),
         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
           RoundedRectangleBorder(
@@ -32,6 +35,8 @@ class HomePage extends StatelessWidget {
         backgroundColor: const MaterialStatePropertyAll(Colors.white),
       ),
       child: Padding(
+        //Acá le estamos diciendo qué es lo que va a contener el botón
+        //En este caso es un texto al que le aplicamos un padding y un style
         padding: const EdgeInsets.all(8.0),
         child: Text(
           text,
@@ -55,6 +60,10 @@ class HomePage extends StatelessWidget {
             children: [
               Text(
                 "Hola $name!",
+                //En Flutter o Dart podemos concatenar Strings con variables de esta forma
+                //Si quisieramos por ejemplo concatenar un String con un int, deberiamos hacerlo de la siguiente forma:
+                // "Hola $name, tu edad es ${edad.toString()}"
+                //Es decir encerrar entre llaves la expresión
                 style: const TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 36,
@@ -77,9 +86,10 @@ class HomePage extends StatelessWidget {
               customButton("Susana", context),
               const SizedBox(height: 10),
               customButton("Miguel", context),
-              const Spacer(
-                flex: 2,
-              ), //Flex 2 es el doble de espacio que flex 1 (por defecto)
+              const Spacer(flex: 2),
+              //Flex 2 es el doble de espacio que flex 1 (por defecto)
+              //Así conseguimos que haya más espacio entre los botones y el botón de volver
+              //Mientras que el espacio entre los botones y el título es más chico
               ElevatedButton(
                 onPressed: () {
                   Navigator.pop(context);
